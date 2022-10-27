9d5494bbf885017ca9bd14f62156b380e6b17382
Chaiwiwat <melondev@icloud.com>
2022-10-28 05:52:03 +0700

    28 Oct 2022

---
+++ lib/constants/ColorConstants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const bgWhiteColor = Color(0xFFFFFFFF);
+const bgButtonColor = Color(0xFF233043);
+const kTextGery = Color(0xFF848484);
+const kTextBlack = Color(0xFF222222);
+const kCardReviwe = Color(0xFFF7F8FA);
+const kBGColor = Color(0xFFFAF7F7);
+
+const bgDisabledTextFieldColor = Color(0xFFE5E5E5);
+
+const buffMenuBGColor = Color(0xff0171BB);
+const buffMenuBGDarkColor = Color(0xff004672);
+
+
